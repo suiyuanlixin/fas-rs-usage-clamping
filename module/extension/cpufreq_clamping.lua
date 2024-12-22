3248578d149ca77145f22ac0af34214db811b9a2
仙道领域绝巅 <ruidawu714@gmail.com>
2024-12-22 19:53:09 +0800

    Add files via upload

--- module/extension/cpufreq_clamping.lua
+++ module/extension/cpufreq_clamping.lua
@@ -1,5 +1,13 @@
 API_VERSION = 0
 
+function start_fas()
+    os.execute("echo 0 > /sys/module/cpufreq_clamping/parameters/enable")
+end
+
+function stop_fas()
+    os.execute("echo 1 > /sys/module/cpufreq_clamping/parameters/enable")
+end
+
 function load_fas()
     os.execute("echo 0 > /sys/module/cpufreq_clamping/parameters/enable")
 end
