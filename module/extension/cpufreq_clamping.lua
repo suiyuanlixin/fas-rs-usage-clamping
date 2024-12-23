103312da74e6167a2a6a2e1aa3bce9dc55a5f90d
仙道领域绝巅 <ruidawu714@gmail.com>
2024-12-23 19:48:57 +0800

    Add files via upload

--- module/extension/cpufreq_clamping.lua
+++ module/extension/cpufreq_clamping.lua
@@ -9,9 +9,11 @@ function stop_fas()
 end
 
 function load_fas()
+    log_info("[cpufreq_clamping] fas-rs load_fas, disable cpufreq_clamping")
     os.execute("echo 0 > /sys/module/cpufreq_clamping/parameters/enable")
 end
 
 function unload_fas()
+    log_info("[cpufreq_clamping] fas-rs unload_fas, enable cpufreq_clamping")
     os.execute("echo 1 > /sys/module/cpufreq_clamping/parameters/enable")
 end
